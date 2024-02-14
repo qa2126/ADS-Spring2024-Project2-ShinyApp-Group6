4736dce5856b30b7d764cc69716e05cd33b1cf23
lesliean <qa2126@columbia.edu>
2024-02-14 00:35:18 -0500

    added the test app

--- app/Leslie-Test.py
+++ app/Leslie-Test.py
@@ -5,44 +5,61 @@ import json
 import dash
 from dash import dcc, html
 from dash.dependencies import Input, Output
+import plotly.express as px
+import os
+
+
+
 
 url = 'https://www.fema.gov/api/open/v1/MissionAssignments'
 
-resp = requests.get(url)
-
-raw = resp.json()
+#resp = requests.get(url)
+#raw = resp.json()
 #print(raw.keys())
+#dat = raw['MissionAssignments']
 
-dat = raw['MissionAssignments']
+script_directory = os.path.dirname(os.path.realpath(__file__))
+parent_directory = os.path.dirname(script_directory)
+file_path = os.path.join(parent_directory, 'data', 'MissionAssignments.csv')
 
-len(dat)
+dat = pd.read_csv(file_path)
+dat.head()
 
-# Create a Dash application
-app_test = dash.Dash(__name__)
+column_names = dat.columns.tolist()
+print(column_names)
 
-# Define the layout of the app
-app_test.layout = html.Div([
-    dcc.Slider(
-        id='input',
-        min=1900,
-        max=2030,
-        step=1,
-        value=2020,
-        marks={str(year): str(year) for year in range(1900, 2031, 10)}
-    ),
-    html.Div(id='output')
+
+dat['dateObligated'] = pd.to_datetime(dat['dateObligated'])
+
+dat['year_month_obligated'] = dat['dateObligated'].dt.strftime('%Y-%m')
+
+
+
+
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.H1("Entries by Month"),
+    dcc.Input(id='selected_year', type='number', min=1900, max=2030, value=2022),
+    html.Div(id='histogram')
 ])
 
-# Define callback to update the output whenever the input changes
-@app_test.callback(
-    Output(component_id='output', component_property='children'),
-    [Input(component_id='input', component_property='value')]
+
+@app.callback(
+    Output('histogram', 'children'),
+    [Input('selected_year', 'value')]
 )
-def update_output_div(input_value):
-    return f'You entered: {input_value}'
+def update_histogram(selected_year):
+    filtered_df = dat[pd.to_datetime(dat['dateObligated']).dt.year == selected_year]
+    if filtered_df.empty:
+        return "No entries for this year"
+    else:
+        fig = px.histogram(filtered_df, x=pd.to_datetime(filtered_df['dateObligated']).dt.month,
+                           labels={'x': 'Month', 'y': 'Number of Entries'},
+                           title=f'Entries by Month in {selected_year}')
+        return dcc.Graph(figure=fig)
 
-# Run the app
+
 if __name__ == '__main__':
-    app_test.run_server(debug=True, port = 8888)
-
-
+    app.run_server(debug=True, port = 8888)
